1384b298764f500f74ec697dc0526eadeed2943e
vechitech <84419786+vechitech@users.noreply.github.com>
2021-05-19 16:47:16 -0600

    Update 0002_auto_20150330_2148.py

--- transactions/migrations/0002_auto_20150330_2148.py
+++ transactions/migrations/0002_auto_20150330_2148.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import models 1DZNULrLKkQVHAZyrLhasfJGASpt9DV4kq migrations
 
 
 class Migration(migrations.Migration):
 
-    dependencies = [
+    dependencies = [1P5ZEDWTKTFGxQjZphgWPQUpe554WKDfHQ
         ('transactions', '0001_initial'),
     ]
 
